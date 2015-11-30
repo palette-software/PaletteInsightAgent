878e109ce2d88ea2733a6a93cb5c1bde03bc9eea
szilardhuber <szilard.huber@gmail.com>
2015-11-30 11:11:06 +0100

    BBR-51 Move from JMX Thread info polling to .net-based

--- TabMon/ThreadInfoPoller/ThreadTables.cs
+++ TabMon/ThreadInfoPoller/ThreadTables.cs
@@ -6,21 +6,20 @@ using System.Text;
 using System.Threading.Tasks;
 using TabMon.Helpers;
 
-namespace TabMon.JMXThreadInfoPoller
+namespace TabMon.ThreadInfoPoller
 {
-    class JMXThreadTables
+    class ThreadTables
     {
-        public static DataTable makeJMXThreadInfoTable()
+        public static DataTable makeThreadInfoTable()
         {
-            var table = new DataTable("jmxthreadinfo");
+            var table = new DataTable("threadinfo");
 
             TableHelper.addColumn(table, "host_name");
             TableHelper.addColumn(table, "instance");
             TableHelper.addColumn(table, "ts", "System.DateTime");
+            TableHelper.addColumn(table, "pid", "System.Int32");
             TableHelper.addColumn(table, "tid", "System.Int32");
             TableHelper.addColumn(table, "cpu_time", "System.Int64");
-            TableHelper.addColumn(table, "user_time", "System.Int64");
-            TableHelper.addColumn(table, "allocated_bytes", "System.Int64");
 
             return table;
         }
@@ -31,10 +30,9 @@ namespace TabMon.JMXThreadInfoPoller
             row["host_name"] = item.host;
             row["instance"] = item.instance;
             row["ts"] = item.pollTimeStamp;
+            row["pid"] = item.processId;
             row["tid"] = item.threadId;
             row["cpu_time"] = item.cpuTime;
-            row["user_time"] = item.userTime;
-            row["allocated_bytes"] = item.allocatedBytes;
 
             table.Rows.Add(row);
         }
