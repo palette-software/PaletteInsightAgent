a69fdfbca7e6216c2a9449d75089aab4f8b5d2e5
Peter Bastyi <baspeti@gmail.com>
2015-12-01 13:10:31 +0100

    Postgres driver hack: timestamp with time zone
    For some reason in TabMon's Postgres driver code someone hard-coded that only the first word of the database type was taken into consideration. This of course screwed our aims to use 'timestamp with time zone' types.

--- DataTableWriter/Drivers/PostgresDriver.cs
+++ DataTableWriter/Drivers/PostgresDriver.cs
@@ -27,6 +27,7 @@ namespace DataTableWriter.Drivers
                 { "smallserial", "System.Int16" },
                 { "text", "System.String" },
                 { "timestamp with time zone", "System.DateTimeOffset" },
+                { "timestamp without time zone", "System.DateTime" },
                 { "timestamp", "System.DateTime" }
             };
 
@@ -94,6 +95,15 @@ namespace DataTableWriter.Drivers
         {
             // We only take the first word of the postgres type into consideration, in order to simplify our mapping.
             var pgFirstTermOfType = pgType.Split(' ')[0].ToLower();
+
+            // HACK-HACK: The line above is nonsense, but now we don't know what is the real cause of taking only
+            // the first word into consideration, so the safe thing is to do here is to add exceptions, like
+            // timestamp with time zone.
+            if (pgFirstTermOfType == "timestamp")
+            {
+                pgFirstTermOfType = pgType;
+            }
+
             if (!postgresToSystemTypeMap.ContainsKey(pgFirstTermOfType))
             {
                 return Type.GetType("System.String");
