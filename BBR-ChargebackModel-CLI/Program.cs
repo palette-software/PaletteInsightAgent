67013bcefda1bfed17c50f6ccbc1ab2a5b4bf3ff
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2015-12-02 16:18:25 +0100

    fix: only revert the database if requested on the CLI

--- BBR-ChargebackModel-CLI/Program.cs
+++ BBR-ChargebackModel-CLI/Program.cs
@@ -20,6 +20,8 @@ namespace BBR_ChargebackModel_CLI
 
         public string ModelFile { get; set; }
         public string ValuesFile { get; set; }
+
+        public bool Revert { get; set; }
     }
 
     class Program
@@ -28,10 +30,16 @@ namespace BBR_ChargebackModel_CLI
         {
             var opts = GetCLIOptions(args);
 
-            // Uncomment this to roll back the DB to its initial state on each run
-            migrator.DbMigrator.MigrateToEmptyDb(opts.DbType, opts.ConnectionString);
+            // If we need to re-migrate, rever the db to its original state
+            if (opts.Revert)
+            {
+                migrator.DbMigrator.MigrateToEmptyDb(opts.DbType, opts.ConnectionString);
+            }
+
+            // Migrate the DB to the current version
             migrator.DbMigrator.MigrateToLatestDbVersion(opts.DbType, opts.ConnectionString);
 
+            // Then import the data
             try
             {
                 importData(opts.DbType, opts.ConnectionString,
@@ -53,18 +61,39 @@ namespace BBR_ChargebackModel_CLI
         /// <param name="args">The list of command line args.</param>
         private static CLIConfig GetCLIOptions(string[] args)
         {
-            if (args.Length < 4)
+            // we may have invalid number of arguments
+            if (args.Length < 4 || args.Length > 5)
             {
-                Console.Out.WriteLine("Usage: BBR-ChargebackModel-CLI <dbtype> <connection_string> <model_file> <values_file>\n\n");
-                Console.Out.WriteLine("  dbtype            : the database type. Can be 'Postgres' or 'SqlServer' or 'Oracle'");
-                Console.Out.WriteLine("  connection_string : The C# ADO connection string for the database");
-                Console.Out.WriteLine("  model_file        : The CSV file containing the chargeback model.");
-                Console.Out.WriteLine("  values_file       : The CSV file containing the hour-by-hour chargeback values.");
-
-                Environment.Exit(-1);
+                DisplayUsageInformationAndExit();
             }
 
-            return new CLIConfig { DbType = args[0], ConnectionString = args[1], ModelFile = args[2], ValuesFile = args[3]};
+            // we may not want to revert
+            if (args.Length == 4)
+            {
+                return new CLIConfig { DbType = args[0], ConnectionString = args[1], ModelFile = args[2], ValuesFile = args[3], Revert = false };
+            }
+
+            // or we may want to revert
+            if (args.Length == 5)
+            {
+                if (args[0] != "revert") DisplayUsageInformationAndExit();
+                return new CLIConfig { DbType = args[1], ConnectionString = args[2], ModelFile = args[3], ValuesFile = args[4], Revert = true };
+            }
+
+            return null;
+
+        }
+
+        private static void DisplayUsageInformationAndExit()
+        {
+            Console.Out.WriteLine("Usage: BBR-ChargebackModel-CLI <revert|> <dbtype> <connection_string> <model_file> <values_file>\n\n");
+            Console.Out.WriteLine("  revert            : When this switch is set, the tables are dropped and re-migrated before run.");
+            Console.Out.WriteLine("  dbtype            : the database type. Can be 'Postgres' or 'SqlServer' or 'Oracle'");
+            Console.Out.WriteLine("  connection_string : The C# ADO connection string for the database");
+            Console.Out.WriteLine("  model_file        : The CSV file containing the chargeback model.");
+            Console.Out.WriteLine("  values_file       : The CSV file containing the hour-by-hour chargeback values.");
+
+            Environment.Exit(-1);
         }
 
 
