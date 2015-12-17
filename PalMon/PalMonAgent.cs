7c563a086df0f5db275b1909055b1f0b789f00ff
Peter Bastyi <bastyip@starschema.net>
2015-12-17 20:32:32 +0100

    I mean debug level

--- PalMon/PalMonAgent.cs
+++ PalMon/PalMonAgent.cs
@@ -217,7 +217,7 @@ namespace PalMon
             if (!Monitor.TryEnter(pollTypeLock, timeout))
             {
                 // Do not execute the poll delegate as it is already being executed.
-                Log.Warn("Skipping poll as it is already in progress: " + pollTypeLock.ToString());
+                Log.Debug("Skipping poll as it is already in progress: " + pollTypeLock.ToString());
                 return;
             }
 
