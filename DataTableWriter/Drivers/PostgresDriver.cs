882adc0f75e075999c01a8ba2bfa5386093ad379
Peter Bastyi <bastyip@starschema.net>
2016-01-13 12:57:34 +0100

    Reversed Postgres driver's ID type change

--- DataTableWriter/Drivers/PostgresDriver.cs
+++ DataTableWriter/Drivers/PostgresDriver.cs
@@ -156,7 +156,7 @@ namespace DataTableWriter.Drivers
         /// <returns>Postgres expression of an identity column specification.</returns>
         public string GetIdentityColumnSpecification()
         {
-            return "id integer";
+            return "id serial PRIMARY KEY";
         }
 
         /// <summary>
