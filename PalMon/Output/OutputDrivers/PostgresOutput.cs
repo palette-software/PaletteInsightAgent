0e13768f82fe811f6f063e186f1cdc56ec5fa2d8
Peter Bastyi <bastyip@starschema.net>
2016-01-20 18:32:05 +0100

    Have the comment in alignment with the code

--- PalMon/Output/OutputDrivers/PostgresOutput.cs
+++ PalMon/Output/OutputDrivers/PostgresOutput.cs
@@ -71,7 +71,7 @@ namespace PalMon.Output
                     foreach (DataRow rowToWrite in rows.Rows)
                     {
                         var row = String.Join("\t", ToTSVLine(rowToWrite.ItemArray));
-                        // if the row ends with a backslash, we need to add another one to it
+                        // if the row ends with a backslash, we need to add a space after it
                         if (row.Last() == '\\') row += " ";
                         writer.WriteLine(row);
                     }
