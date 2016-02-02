988fd846cfea7a378d49e9fe0b1c0aae75b7566f
szilardhuber <szilard.huber@gmail.com>
2016-02-02 15:16:15 +0100

    Fixing some publisher and license information.

--- LicenseGenerator/Properties/AssemblyInfo.cs
+++ LicenseGenerator/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LicenseGenerator")]
+[assembly: AssemblyTitle("Palette Insight License Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("LicenseGenerator")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCompany("Palette Software")]
+[assembly: AssemblyProduct("Palette Insight")]
+[assembly: AssemblyCopyright("Copyright © Palette Software 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Licensing/Properties/AssemblyInfo.cs
+++ Licensing/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Licensing")]
+[assembly: AssemblyTitle("Palette Insight Licensing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Licensing")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCompany("Palette Software")]
+[assembly: AssemblyProduct("Palette Insight Agent")]
+[assembly: AssemblyCopyright("Copyright © Palette Software 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
