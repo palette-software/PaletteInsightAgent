87cfa1b9857a5d3eebae677f4487205949003204
Peter Bastyi <bastyip@starschema.net>
2016-02-10 18:40:55 +0100

    Fixed calculation of rows written to database

--- PalMon/Helpers/LoggingHelpers.cs
+++ PalMon/Helpers/LoggingHelpers.cs
@@ -10,12 +10,11 @@ namespace PalMon.Helpers
     public class LoggingHelpers
     {
 
-        public static void TimedLog(Logger Log, string message, Action<int> act)
+        public static void TimedLog(Logger Log, string message, Func<int> act)
         {
             Log.Info("--> Starting {0}", message);
             var currentTime = DateTime.UtcNow;
-            int rowsWritten = 0;
-            act(rowsWritten);
+            int rowsWritten = act();
             var endTime = DateTime.UtcNow;
             Log.Info("<-- Done [{1}ms] {0}. Total rows written: {2}", message,
                 (endTime - currentTime).TotalMilliseconds, rowsWritten);
