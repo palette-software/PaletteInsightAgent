c28aeed39fe5f841e33ffbef8de9c5127654c0c3
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-02-25 16:00:05 +0100

    fix: added missing project file, added missing requires

--- PaletteInsightAgent/Output/OutputDrivers/WebserviceSinglefileBackend.cs
+++ PaletteInsightAgent/Output/OutputDrivers/WebserviceSinglefileBackend.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
