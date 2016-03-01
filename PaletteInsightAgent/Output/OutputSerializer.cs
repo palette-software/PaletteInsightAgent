1c17bb01b488e0afdd4f8b3acad85967c427a4b4
szilardhuber <szilard.huber@gmail.com>
2016-03-01 16:59:59 +0100

    Changed csv format and removed serverlog parsing.

--- PaletteInsightAgent/Output/OutputSerializer.cs
+++ PaletteInsightAgent/Output/OutputSerializer.cs
@@ -3,14 +3,26 @@ using System.IO;
 using System.Data;
 using NLog;
 using System.Globalization;
+using System.Collections.Generic;
+using Microsoft.Hadoop.Avro;
+using Microsoft.Hadoop.Avro.Schema;
 
 namespace PaletteInsightAgent.Output
 {
-    class CsvOutput
+    class OutputSerializer
     {
-        private const string CSV_DATETIME_FORMAT = "yyyy-MM-dd--HH-mm-ss";
+        public const string DATA_FOLDER = "data/";
+        private const string FILENAME_DATETIME_FORMAT = "yyyy-MM-dd--HH-mm-ss";
         public  const string IN_PROGRESS_FILE_POSTFIX = ".writing";
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private static IWriter Writer = new CsvSerializer();
+        public static string Extension
+        {
+            get
+            {
+                return Writer.Extension;
+            }
+        }
 
         public static void Write(DataTable table)
         {
@@ -25,20 +37,20 @@ namespace PaletteInsightAgent.Output
             try
             {
                 // write out if any
-                var csvFileName = GetCSVFile(table.TableName);
+                var dataFileName = GetDataFile(table.TableName);
 
                 // First create the file name with a postfix, so that the bulk copy
                 // loader won't touch this file, until it is being written.
-                var inProgressCsvFileName = csvFileName + IN_PROGRESS_FILE_POSTFIX;
+                var inProgressFileName = dataFileName + IN_PROGRESS_FILE_POSTFIX;
 
-                WriteCsvFile(inProgressCsvFileName, table);
+                Writer.WriteDataFile(inProgressFileName, table);
 
                 // remove any rows from the csv queue
                 table.Rows.Clear();
 
                 // Remove the postfix to signal that the file write is done.
-                File.Move(inProgressCsvFileName, csvFileName);
-                Log.Info("{0} {1} written to CSV file: {2}", rowCount, "row".Pluralize(rowCount), csvFileName);
+                File.Move(inProgressFileName, dataFileName);
+                Log.Info("{0} {1} written to CSV file: {2}", rowCount, "row".Pluralize(rowCount), dataFileName);
             }
             catch (Exception e)
             {
@@ -52,11 +64,11 @@ namespace PaletteInsightAgent.Output
         /// </summary>
         /// <param name="fileBaseName"></param>am>
         /// <returns></returns>
-        private static string GetCSVFile(string fileBaseName)
+        private static string GetDataFile(string fileBaseName)
         {
-            var dateString = DateTimeOffset.Now.UtcDateTime.ToString(CSV_DATETIME_FORMAT);
+            var dateString = DateTimeOffset.Now.UtcDateTime.ToString(FILENAME_DATETIME_FORMAT);
             // get a new filename
-            var fileName = String.Format("csv/{0}-{1}.csv", fileBaseName, dateString);
+            var fileName = String.Format("{0}{1}-{2}{3}", DATA_FOLDER, fileBaseName, dateString, Writer.Extension);
 
             // try to create the directory of the output
             try
@@ -69,29 +81,6 @@ namespace PaletteInsightAgent.Output
             }
 
             return fileName;
-        }
-
-        /// <summary>
-        /// Writes the CSV out. This method appends to the file if it
-        /// already exists so we can just add more rows while 
-        /// making sure the file is flushed and closed after each write
-        /// </summary>
-        /// <param name="fileName"></param>
-        private static void WriteCsvFile(string fileName, DataTable table)
-        {
-            var fileExists = File.Exists(fileName);
-
-            using (var streamWriter = File.AppendText(fileName))
-            using (var csvWriter = new CsvHelper.CsvWriter(streamWriter, new CsvHelper.Configuration.CsvConfiguration { CultureInfo = CultureInfo.InvariantCulture }))
-            {
-                // only write the header if the file does not exists
-                if (!fileExists)
-                {
-                    DataTableUtils.WriteCSVHeader(table, csvWriter);
-                }
-
-                DataTableUtils.WriteCSVBody(table, csvWriter);
-            }
         }
 
         #region IDisposable Support
