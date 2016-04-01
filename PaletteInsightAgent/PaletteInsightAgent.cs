c264a2a5cb262f998d817f99600994c1fb54fdc7
Peter Bastyi <bastyip@starschema.net>
2016-04-01 14:25:32 +0200

    Stop hack for SplunkNLog target

--- PaletteInsightAgent/PaletteInsightAgent.cs
+++ PaletteInsightAgent/PaletteInsightAgent.cs
@@ -59,6 +59,8 @@ namespace PaletteInsightAgent
         private bool USE_WEBSERVICE = false;
         private bool USE_TABLEAU_REPO = true;
 
+        public static bool STOPPING = false;
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed")]
         public PaletteInsightAgent(bool loadOptionsFromConfig = true)
         {
@@ -262,6 +264,7 @@ namespace PaletteInsightAgent
         public void Stop()
         {
             Log.Info("Shutting down PaletteInsightAgent..");
+            STOPPING = true;
 
             if (USE_COUNTERSAMPLES)
             {
