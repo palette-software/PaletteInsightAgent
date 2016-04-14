a6fbb3d40e71ea00365a1ff57d026cc26e47b663
szilardhuber <szilard.huber@gmail.com>
2016-04-14 17:31:52 +0200

    Kill OutputWriteResult \o/

--- PaletteInsightAgent/Output/OutputDrivers/IOutput.cs
+++ PaletteInsightAgent/Output/OutputDrivers/IOutput.cs
@@ -9,54 +9,7 @@ namespace PaletteInsightAgent.Output
 {
 
     /// <summary>
-    /// An output wrapper for the results of an IOutput.Write().
-    /// </summary>
-    public class OutputWriteResult
-    {
-        /// <summary>
-        /// Files that were successfully written to the database
-        /// </summary>
-        public IList<string> successfullyWrittenFiles = new List<string>();
-        /// <summary>
-        /// Files that failed with unrecoverable errors
-        /// </summary>
-        public IList<string> failedFiles = new List<string>();
-
-        /// <summary>
-        /// Combines the contents of two output results
-        /// </summary>
-        /// <param name="parts"></param>
-        public static OutputWriteResult Combine(params OutputWriteResult[] parts)
-        {
-            return parts.Aggregate(new OutputWriteResult(), (memo,part)=>{
-                memo.failedFiles.AddRange(part.failedFiles);
-                memo.successfullyWrittenFiles.AddRange(part.successfullyWrittenFiles);
-                return memo;
-            });
-        }
-
-
-        public static OutputWriteResult Aggregate<T>(IEnumerable<T> seq, Func<T, OutputWriteResult> fn )
-        {
-            return seq.Aggregate(new OutputWriteResult(), (memo, res) => Combine(memo, fn(res)));
-        }
-
-        #region quickcreate
-        public static OutputWriteResult Ok(params string[] files)
-        {
-            return new OutputWriteResult { successfullyWrittenFiles = new List<string>(files) };
-        }
-
-        public static OutputWriteResult Failed(params string[] files)
-        {
-            return new OutputWriteResult { failedFiles = new List<string>(files) };
-        }
-
-        #endregion
-    }
-
-    /// <summary>
-    /// A generic interface for writing to the database.
+    /// A generic interface for writing data
     ///  NOTE: since we (possibly) need to close files and/or database connections,
     ///  instances of this interface has to extend IDisposable
     /// </summary>
