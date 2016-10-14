53fed8f81b4af9a3f02432f89726538158ef52dd
Peter Bastyi <peter@palette-software.com>
2016-10-14 13:11:44 +0200

    Prototype of using variable set via the installer UI

---
+++ PaletteInsightFillConfig/FillConfigAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+using System.Windows.Forms;
+
+namespace PaletteInsightFillConfig
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult FillConfig(Session session)
+        {
+            session.Log("Begin FillConfig");
+            var InsightServerUrl = session["WIXUI_INSIGHTSERVERURL"];
+
+            MessageBox.Show("Insight Server URL: ", InsightServerUrl);
+            return ActionResult.Success;
+        }
+    }
+}
