a5f2754d7774baaf2cf2b68777fddd1ffd4641f0
Peter Bastyi <peter@palette-software.com>
2016-10-15 15:21:29 +0200

    Use msi logger for our FillConfig custom action (the /log parameter of msiexec)

--- PaletteInsightFillConfig/FillConfigAction.cs
+++ PaletteInsightFillConfig/FillConfigAction.cs
@@ -13,66 +13,64 @@ namespace PaletteInsightFillConfig
         [CustomAction]
         public static ActionResult FillConfig(Session session)
         {
-            var logFilePath = "msi.log";
-            using (TextWriter logWriter = File.CreateText(logFilePath))
+            try
             {
-                try
+                session.Log("Filling up Palette Insight Agent config file based on the parameters specified during the installer UI.");
+                bool anyChangeOnUI = false;
+
+                string installFolder = session.CustomActionData["InstallDir"];
+                var configFilePath = Path.Combine(installFolder, "Config", "Config.yml");
+                session.Log("Insight agent's config file path: {0}", configFilePath);
+
+                string insightServerUrl = session.CustomActionData["InsightServerUrl"];
+                session.Log("Insight Server URL: '{0}'", insightServerUrl);
+
+                string insightLicenseKey = session.CustomActionData["InsightLicenseKey"];
+                session.Log("License key: '{0}'", insightLicenseKey);
+
+                var agentConfig = Loader.LoadConfigFile(configFilePath);
+                if (agentConfig == null)
                 {
-                    session.Log("Begin FillConfig");
-                    bool anyChangeOnUI = false;
-
-                    logWriter.Write("Acquiring Insight Server URL...\n");
-                    string insightServerUrl = session.CustomActionData["InsightServerUrl"];
-                    logWriter.Write(String.Format("Insight Sercer URL: {0}\n", insightServerUrl));
-                    logWriter.Write("Acquiring install folder...\n");
-
-                    string installFolder = session.CustomActionData["InstallDir"];
-                    var configFilePath = Path.Combine(installFolder, "Config", "Config.yml");
-                    logWriter.Write(String.Format("Config file path: {0}\n", configFilePath));
-
-                    string insightLicenseKey = session.CustomActionData["InsightLicenseKey"];
-                    logWriter.Write(String.Format("License key: {0}\n", insightLicenseKey));
-
-                    var agentConfig = Loader.LoadConfigFile(configFilePath);
-                    if (agentConfig == null)
-                    {
-                        logWriter.Write(String.Format("Failed to load config file at path: {0}\n", configFilePath));
-                        return ActionResult.Failure;
-                    }
-
-                    if (insightLicenseKey != "")
-                    {
-                        agentConfig.LicenseKey = insightLicenseKey;
-                        anyChangeOnUI = true;
-                    }
-
-                    if (insightServerUrl != "" && insightServerUrl != "https://")
-                    {
-                        agentConfig.Webservice.Endpoint = insightServerUrl;
-                        anyChangeOnUI = true;
-                    }
-
-
-                    if (anyChangeOnUI)
-                    {
-                        // Only modify the Config.yml, if there is any change. This is very important,
-                        // so that silent installs never overwrite the config file.
-                        using (TextWriter configWriter = File.CreateText(configFilePath))
-                        {
-                            var serializer = new Serializer();
-                            serializer.Serialize(configWriter, agentConfig);
-                        }
-                    }
-
-                    logWriter.Write("We are gooood!\n");
-                }
-                catch (Exception e)
-                {
-                    logWriter.Write(String.Format("Failed to apply UI based settings to config file! Exception: {0}\n", e));
+                    session.Log("Failed to load config file at path: {0}", configFilePath);
                     return ActionResult.Failure;
                 }
+
+                if (insightLicenseKey != "")
+                {
+                    agentConfig.LicenseKey = insightLicenseKey;
+                    anyChangeOnUI = true;
+                    session.Log("Set LicenseKey to: {0}", insightLicenseKey);
+                }
+
+                if (insightServerUrl != "" && insightServerUrl != "https://")
+                {
+                    agentConfig.Webservice.Endpoint = insightServerUrl;
+                    anyChangeOnUI = true;
+                    session.Log("Set Webservice.Endpoint to: {0}", insightServerUrl);
+                }
+
+
+                if (anyChangeOnUI)
+                {
+                    // Only modify the Config.yml, if there is any change. This is very important,
+                    // so that silent installs never overwrite the config file.
+                    using (TextWriter configWriter = File.CreateText(configFilePath))
+                    {
+                        var serializer = new Serializer();
+                        serializer.Serialize(configWriter, agentConfig);
+                    }
+                    session.Log("Successfully applied changes on Insight Agent's config file.");
+                }
+                else
+                {
+                    session.Log("No changes were proposed for the Insight Agent's config file. Leave it untouched.");
+                }
             }
-            
+            catch (Exception e)
+            {
+                session.Log("Failed to apply UI based settings to config file! Exception: {0}", e);
+                return ActionResult.Failure;
+            }
 
             return ActionResult.Success;
         }
