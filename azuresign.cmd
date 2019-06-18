66cbf6e6767571006a21fc874a3df4ccf924939e
SELLER Peter Andras <petispaespea@gmail.com>
2019-06-18 10:29:57 +0200

    Sign installer with azure sign

---
+++ azuresign.cmd
@@ -0,0 +1,10 @@
+@echo off
+dotnet tool install --global AzureSignTool
+azuresigntool sign --description-url "https://starschema.com" \
+    --file-digest sha256 --azure-key-vault-url https://billiant-keyvault.vault.azure.net \
+    --azure-key-vault-client-id %AZURE_KEY_VAULT_CLIENT_ID% \
+    --azure-key-vault-client-secret %AZURE_KEY_VAULT_CLIENT_SECRET% \
+    --azure-key-vault-certificate StarschemaCodeSigning \
+    --timestamp-rfc3161 http://timestamp.digicert.com --timestamp-digest sha256 \
+    --no-page-hashing \
+    %*
